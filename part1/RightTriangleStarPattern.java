12253df40518fb69dfa29f444b935fb009ed7121
burcu-f <burcufyz@gmail.com>
2023-10-21 20:18:38 +0300

    Right Triangle Star Pattern added

--- part1/RightTriangleStarPattern.java
+++ part1/RightTriangleStarPattern.java
@@ -4,6 +4,7 @@ public class RightTriangleStarPattern {
     public static void main(String[] args) {
         int i, j, n = 6;
 
+
         for (i = 0; i < n; i++) {
             for (j = 0; j <= i; j++){
                 System.out.print("* ");
