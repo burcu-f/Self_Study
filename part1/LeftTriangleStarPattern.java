caf5b86a69c6c6096960403ceef21ee0fea40fae
burcu-f <burcufyz@gmail.com>
2023-10-23 14:34:20 +0300

    Left Triangle Star Pattern added

---
+++ part1/LeftTriangleStarPattern.java
@@ -0,0 +1,17 @@
+package part1;
+
+public class LeftTriangleStarPattern {
+    public static void main(String[] args) {
+        int i, j, n = 6;
+
+        for (i = 0; i < n; i++){
+            for (j = 2 * (n-i); j >= 0; j--){
+                System.out.print(" ");
+            }
+            for (j = 0; j <= i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
