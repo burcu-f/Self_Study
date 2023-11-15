9ce07560f4657f125adc66ea834d6f27faf0bfb9
burcu-f <burcufyz@gmail.com>
2023-11-15 17:45:40 +0300

    Object-Oriented Programming Exercise (ATM) Added

---
+++ OOPatm/Main.java
@@ -0,0 +1,16 @@
+package OOPatm;
+
+public class Main {
+    public static void main(String[] args) {
+        ATM atm = new ATM();
+
+        Hesap hesap = new Hesap("Burcu","12345",2000);
+
+            atm.calis(hesap);
+
+            System.out.println("Programdan Çıkılıyor...");
+
+
+
+    }
+}
